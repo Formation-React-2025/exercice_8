451e6219ccdfcb39a9dc4f12978330e8cbcafa44
Thibault Bondetti <thibault.bondetti@cgi.com>
2025-06-24 10:56:37 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Exercice7 from './components/exercices/exercice-7/Exercice7';
+
+const App = () => (
+  <main>
+    <Exercice7 />
+  </main>
+);
+
+export default App;
